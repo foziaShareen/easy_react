9ba8052d3c01ceb758b8c7a9c97727818fb1d156
foziaShareen <foziashareen1@gmail.com>
2021-05-20 13:53:12 +0500

    class2 branch

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// const element = <h1 className="styleclass">Welcome to react programming class day one </h1>
-// ReactDOM.render(element,document.getElementById('root'));
-//if we want to make two h1 what will wwe do
-// const newelement= <h1 className="styleclass">Welcome to react programming class day one </h1>
-// const element = <h1 className="styleclass">hello to all</h1>
-// ReactDOM.render(element,document.getElementById('root'));
-// ReactDOM.render(element,document.getElementById('root2'));
-//it is working but it is not the solution
-// const element=(
-//   <div className="styleclass">git
-//     <h1>welcome to the class day one</h1>
-//     <h1>hello to the class day two</h1>
-//   </div>
+function StudentInfo(student){
+  return(
+<div className="styleclass">
+  <p>
+    <label>
 
-// );
-const element =(
-  React.createElement('div',{className:"styleclass"},React.createElement('h1',null,"welcome to all of you"),
-  React.createElement('h1',null,"hello to all of you"))
-)
+      student Roll_No:{student.Roll_No}
+      
+    </label>
+  </p>
+  <p>
+    <label>
+
+      student Roll_No:{student.Name}
+      
+    </label>
+  </p>
+  <p>
+    <label>
+
+      student Roll_No:{student.Class}
+      
+    </label>
+  </p>
+</div>)
+}
+const element =<StudentInfo Roll_No="1" Name="Mansoor" Class="final"/>
+ 
+
 ReactDOM.render(element,document.getElementById('root'))
 
