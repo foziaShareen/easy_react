a00c72b8b62a80bcef7d02dffa809e85d206e9c8
foziaShareen <foziashareen1@gmail.com>
2021-05-24 16:17:20 +0500

    function call from parent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import Student  from './salary'
 import React ,{useState} from 'react'
-import Login from './Login'
+import User from './User'
 
 function App() {
+    function getData(){
+    return(
+      
+    alert("hello")
+    
+    )
+    //why we make function and then call from child?
+    // because may be we want to use in many children and it is convenient to define only 
+    // once in parent and call from many child component as we want to be .
+}
  return(
    <div>
-     <Login/>
+    <User data={getData}/>
    </div>
  )
 }
---
+++ src/User.js
@@ -0,0 +1,12 @@
+import React from 'react'
+ 
+function User(props){
+  
+    return(<div>
+        
+<button onClick={props.data}>click</button>
+    </div>
+
+    )
+}
+export default User
