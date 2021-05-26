00091e1836dcf62581435a8b4a687d15351005fd
foziaShareen <foziashareen1@gmail.com>
2021-05-26 16:34:29 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import Student  from './salary'
-import React ,{useState} from 'react'
+import React ,{useEffect,useState} from 'react'
 
 function App() {
-   const[Data,setData]=useState("shareen");
-    
+   
+  const[count,setCount]=useState(0)  
+   useEffect(() => {
+    console.warn("use effect")
+   })
+      
+     
 
  return(
    <div>
-    <h1>{Data}</h1>
-    <button onClick={()=>setData("fozia")}>click</button>
+  <h1>useEffect in React</h1>
+  <h1>Count= {count}</h1>
+  <button onClick={()=>setCount(count+1)}>update</button> 
    </div>
  )
  }
